098c28cf69cbb18a32c18c32bf65502f3c46dfde
Matheus <matheusmartinscia@gmail.com>
2025-07-08 20:11:52 -0300

    Feat: Cria links para login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,9 +2,9 @@
     <x-slot:content>
         <div class="d-flex justify-content-center gap-5 mt-5">
 
-            <a href="{{ route('login_user', ['id' => 2]) }}" class="btn btn-large btn-outline-primary px-5">Login Admin</a>
-            <a href="{{ route('login_user', ['id' => 3]) }}" class="btn btn-large btn-outline-primary px-5">Login User</a>
-            <a href="{{ route('login_user', ['id' => 4]) }}" class="btn btn-large btn-outline-primary px-5">Login Visitor</a>
+            <a href="{{ route('login_user', ['id' => 1]) }}" class="btn btn-large btn-outline-primary px-5">Login Admin</a>
+            <a href="{{ route('login_user', ['id' => 2]) }}" class="btn btn-large btn-outline-primary px-5">Login User</a>
+            <a href="{{ route('login_user', ['id' => 3]) }}" class="btn btn-large btn-outline-primary px-5">Login Visitor</a>
 
         </div>
     </x-slot:content>
