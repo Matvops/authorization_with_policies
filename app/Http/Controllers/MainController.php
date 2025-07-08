091f6952a6f6e90e3940cc66964baf37b792f1d2
Matheus <matheusmartinscia@gmail.com>
2025-07-08 20:12:10 -0300

    Feat: Altera echo por view, no método index

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class MainController extends Controller
 {
-    public function index() {
-        echo "Hello world";
+    public function index(): View
+    {
+        return view('home');
     }
 }
